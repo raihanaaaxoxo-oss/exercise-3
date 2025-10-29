4139f52c9e853f743934d17b287ad9621f97dadd
raihanaaaxoxo-oss <raihanaaaxoxo@gmail.com>
2025-10-29 15:33:39 +0200

    initial commit

---
+++ exercise_3_arrays.js
@@ -0,0 +1,10 @@
+let colors = []
+colors.push("beige", "magenta", "olive green")
+
+let numbers = [1, 2, 3, 4, 5] 
+
+colors.pop()
+colors.unshift("turquoise")
+
+console.log("the length of the colors array is " + colors.length)
+console.log("the length of the numbers array is " + numbers.length) 
